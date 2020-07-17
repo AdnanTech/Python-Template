812dec7780342b2220ded40bf03f920269bbe212
Adnan Tech <adnanquisar0@gmail.com>
2020-07-17 16:40:20 +0100

    Ver 2
    Completed

--- python-template.py
+++ python-template.py
@@ -1,11 +1,15 @@
-
-
 from datetime import datetime
+
 start_time = datetime.now()
 
 def initialize():
     print('Starting')
 
+def main():
+    print('Code goes here')
+
 initialize()
+main()
+
 end_time = datetime.now()
 print('Duration: {}'.format(end_time - start_time))
